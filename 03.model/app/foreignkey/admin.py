c6da6543d42cad110db14fdcbf8d25319fea6bed
LeeHanYeong <dev@lhy.kr>
2019-11-23 17:33:39 +0900

    foreignkey

--- 03.model/app/foreignkey/admin.py
+++ 03.model/app/foreignkey/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment, DjangoStudyMember
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DjangoStudyMember)
+class DjangoStudyMemberAdmin(admin.ModelAdmin):
+    pass
--- 03.model/app/foreignkey/models.py
+++ 03.model/app/foreignkey/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField('제목', max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField('내용', blank=True)
+
+    def __str__(self):
+        return f'{self.post.title}글 의 댓글 {self.content}'
+
+
+class DjangoStudyMember(models.Model):
+    name = models.CharField('이름', max_length=20)
+    leader = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
