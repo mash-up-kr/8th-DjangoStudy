1631c15bee21b7d49e1397a3edea2d7f86a11e64
이한영 <dev@lhy.kr>
2019-12-21 12:34:36 +0900

    update

--- 05.query/app/config/settings.py
+++ 05.query/app/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'query',
+
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- 05.query/app/query/models.py
+++ 05.query/app/query/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField(blank=True)
+    pub_date = models.DateField(blank=True, null=True)
+    mod_date = models.DateField(blank=True, null=True)
+    authors = models.ManyToManyField(Author, blank=True)
+    number_of_comments = models.IntegerField(default=0)
+    number_of_pingbacks = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.headline
